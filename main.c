e4caf3f8561c21a5f0bdaaa375e6409d9579008b
samsamsam <samsamsam@o2.pl>
2021-04-05 10:38:06 +0200

    Add possibility to set configuration of NEC

--- main.c
+++ main.c
@@ -14,6 +14,14 @@
 
 typedef struct
 {
+    uint8_t address_l;
+    uint8_t address_h;
+    uint8_t power_key;
+    uint8_t flags;
+} avr_config;
+
+typedef struct
+{
     char name[256];
     char serial[256];
     char uinput[256];
@@ -22,9 +30,27 @@ typedef struct
     bool reset;         /* request reset at start */
     bool debug;         /* enable debug */
     bool power_confirm; /* power confirmation */
+    bool check_avr_cfg;
 
+    avr_config avr_cfg;
     uint16_t key_map[IR_MAX_KEYS_NUM];
 } configuration;
+
+typedef struct {
+    uint8_t address_l;
+    uint8_t address_h;
+    uint8_t power_key;
+    uint8_t flags;
+    uint8_t hash;
+} avr_scfg_t;
+
+
+typedef union {
+    avr_scfg_t s;
+    uint8_t data[0];
+} avr_cfg_t;
+
+int usleep(unsigned int);
 
 static void config_error(const char *section, const char *name, const char *value)
 {
@@ -35,7 +61,46 @@ static int config_handler(void *ptr, const char *section, const char *name, cons
 {
     configuration *pconfig = (configuration*)ptr;
 
-    if (0 == strcmp(section, "config"))
+    if (0 == strcmp(section, "avr_cfg"))
+    {
+        if (0 == strcmp(name, "address_l"))
+        {
+            if (0 == sscanf(value, "%hhu", &(pconfig->avr_cfg.address_l)))
+            {
+                config_error(section, name, value);
+                return 0;
+            }
+        }
+
+        else if (0 == strcmp(name, "address_h"))
+        {
+            if (0 == sscanf(value, "%hhu", &(pconfig->avr_cfg.address_h)))
+            {
+                config_error(section, name, value);
+                return 0;
+            }
+        }
+
+        else if (0 == strcmp(name, "power_key"))
+        {
+            if (0 == sscanf(value, "%hhu", &(pconfig->avr_cfg.power_key)))
+            {
+                config_error(section, name, value);
+                return 0;
+            }
+        }
+
+        else if (0 == strcmp(name, "flags"))
+        {
+            if (0 == sscanf(value, "%hhu", &(pconfig->avr_cfg.flags)))
+            {
+                config_error(section, name, value);
+                return 0;
+            }
+        }
+
+    }
+    else if (0 == strcmp(section, "config"))
     {
         if (0 == strcmp(name, "serial"))
         {
@@ -91,6 +156,15 @@ static int config_handler(void *ptr, const char *section, const char *name, cons
                 return 0;
             }
             pconfig->power_confirm = value[0] == '1' ? true : false;
+        }
+        else if (0 == strcmp(name, "check_avr_cfg"))
+        {
+            if (0 != strcmp(value, "1") && 0 != strcmp(value, "0"))
+            {
+                config_error(section, name, value);
+                return 0;
+            }
+            pconfig->check_avr_cfg = value[0] == '1' ? true : false;
         }
     }
     else if (0 == strcmp(section, "keymap"))
@@ -193,6 +267,7 @@ int main(int argc, char *argv[])
         config.key_map[i] = (uint16_t)-1; // set all event code's as UNKNOWN
     }
 
+
     if (argc > 1)
     {
         if (0 == strcmp(argv[1], "--help"))
@@ -249,7 +324,7 @@ int main(int argc, char *argv[])
     if (config.reset)
     {
         /* request reset at start */
-        write(fd, "RsT", 3);
+        write(fd, ".RsT", 4);
     }
 
     if (1 || uinputfd > -1)
@@ -280,7 +355,80 @@ int main(int argc, char *argv[])
                         {
                             printf("%s\n", buf);
                         }
-                        process_key_message(uinputfd, buf, config.power_confirm ? fd : -1);
+
+                        if (0 == strncmp(buf, "CfK", 3) || 0 == strncmp(buf, "CfO", 3))
+                        {
+                            write(fd, ".RsT", 4);
+                            config.check_avr_cfg = false;
+                        }
+                        else if (0 == strncmp(buf, "CfS", 3))
+                        {
+                            int i = 0;
+                            avr_cfg_t avr_cfg;
+                            avr_cfg.s.address_l = config.avr_cfg.address_l;
+                            avr_cfg.s.address_h = config.avr_cfg.address_h;
+                            avr_cfg.s.power_key = config.avr_cfg.power_key;
+                            avr_cfg.s.flags = config.avr_cfg.flags;
+                            avr_cfg.s.hash = 0xAA;
+                            for (i = 0; i < sizeof(avr_cfg)-1; ++i)
+                            {
+                                avr_cfg.s.hash ^= avr_cfg.data[i];
+                            }
+
+                            write(fd, avr_cfg.data, sizeof(avr_cfg));
+                        }
+                        else if (0 == strncmp(buf, "StArT", 5))
+                        {
+                            avr_cfg_t avr_cfg;
+                            size_t rsize = 0;
+                            while (rsize < sizeof(avr_cfg))
+                            {
+                                rsize += read(fd, avr_cfg.data + rsize, sizeof(avr_cfg) - rsize);
+                                usleep(100);
+                            }
+
+                            if (sizeof(avr_cfg) == rsize)
+                            {
+                                if (config.debug)
+                                {
+                                    uint8_t hash = 0xAA;
+                                    int i = 0;
+                                    for (i = 0; i < sizeof(avr_cfg)-1; ++i)
+                                    {
+                                        hash ^= avr_cfg.data[i];
+                                    }
+                            
+                                    printf("AVR config received:\n");
+                                    printf("\taddress_l:%hhu\n", avr_cfg.s.address_l);
+                                    printf("\taddress_h:%hhu\n", avr_cfg.s.address_h);
+                                    printf("\tpower_key:%hhu\n", avr_cfg.s.power_key);
+                                    printf("\tflags:%hhu\n", avr_cfg.s.flags);
+                                    printf("\thash:%hhu\n", avr_cfg.s.hash);
+                                    printf("\tcalc hash:%hhu\n", hash);
+                                }
+                                if (config.check_avr_cfg)
+                                {
+                                    if (config.avr_cfg.address_l != avr_cfg.s.address_l ||
+                                        config.avr_cfg.address_h != avr_cfg.s.address_h ||
+                                        config.avr_cfg.power_key != avr_cfg.s.power_key ||
+                                        config.avr_cfg.flags != avr_cfg.s.flags)
+                                    {
+                                        if (config.debug)
+                                        {
+                                            printf("AVR has wrong config try to set correct one!\n");
+                                            write(fd, ".CfG", 4);
+                                        }
+                                    }
+                                    //write(fd, "RsT", 3);
+                                }
+                            }
+                            else
+                                printf("WRONG CONFIG SIZE: %u, EXPECTED: %u\n", rsize, sizeof(avr_cfg));
+                        }
+                        else 
+                        {
+                            process_key_message(uinputfd, buf, config.power_confirm ? fd : -1);
+                        }
                     }
                     rlen = 0;
                 }
